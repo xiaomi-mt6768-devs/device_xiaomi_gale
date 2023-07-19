1c85e440fdddb8ffcf62cf784013049bd3577818
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-19 10:50:14 +0100

    gale: Build default health HIDL
    Change-Id: I72bf9b21274d27952997b9ddcb3f1ae1a7ad8013

--- device.mk
+++ device.mk
@@ -37,6 +37,12 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6768 \
