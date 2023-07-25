24a0729948891a138bab3189f75835660821a0b0
bengris32 <bengris32@protonmail.ch>
2023-07-25 12:59:56 +0100

    gale: Import Widevine DRM blobs
    Change-Id: I93ba95e0d5174689880ce5b7ab611fe1c2ba5637

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_PACKAGES += \
     libdrm.vendor \
     libutils-v32
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
