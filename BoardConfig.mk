b1d016bdeb37e9a91b4e17a14aa7a68b8c3a5800
sabrina010209 <mcpe11640@gmail.com>
2024-08-16 15:46:02 -0500

    gale: define bootloader and platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,5 +20,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := gale
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6769
+BOARD_HAS_MTK_HARDWARE := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/gale/BoardConfigVendor.mk
