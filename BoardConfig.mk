caeb4199e4e409801e321d2a79f1b4ca6f872461
bengris32 <bengris32@protonmail.ch>
2024-08-18 06:46:11 -0500

    gale: Generate FCM
    *Made with https://github.com/afterallafk/fcm_generator

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,8 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Inherit the proprietary files
 include vendor/xiaomi/gale/BoardConfigVendor.mk
