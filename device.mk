b660da5fc7a1b491b92b20fffd68ea41000d790a
bengris32 <bengris32@protonmail.ch>
2024-08-23 09:17:57 +0530

    gale: Switch to AOSP audio service & Import missing blobs
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -47,8 +47,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service.mediatek
-
+    android.hardware.audio.service \
+    android.hardware.bluetooth.audio-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
