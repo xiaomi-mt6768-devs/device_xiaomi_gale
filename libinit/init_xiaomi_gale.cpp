4be6e393c1c977be2e14055928e89e29b7382e2c
ziadtamer11 <ziadtamer150@gmail.com>
2024-09-25 12:24:03 -0500

    gale: libinit: Set `ro.product.mod_device` property
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I85baa2a62d79526737b5d65aa93f9042262ae6a0
    Signed-off-by: ziadtamer11 <ziadtamer150@gmail.com>

--- libinit/init_xiaomi_gale.cpp
+++ libinit/init_xiaomi_gale.cpp
@@ -8,79 +8,85 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t gust_info = {
+static const variant_info_t c3un_info = {
     .hwc_value = "",
-    .sku_value = "gust",
+    .sku_value = "c3un",
 
     .brand = "Redmi",
     .device = "gust",
+    .mod_device = "gust_global",
     .marketname = "Redmi 13C",
     .model = "23108RN04Y",
     .build_fingerprint = "Redmi/gale_global/gale:13/SP1A.210812.016/V816.0.2.0.UGPMIXM:user/release-keys"
 };
 
-static const variant_info_t gale_info = {
+static const variant_info_t c3ua_info = {
     .hwc_value = "",
-    .sku_value = "gale",
+    .sku_value = "c3ua",
 
     .brand = "Redmi",
     .device = "gale",
+    .mod_device = "gale_global",
     .marketname = "Redmi 13C",
     .model = "23106RN0DA",
     .build_fingerprint = "Redmi/gale_global/gale:13/SP1A.210812.016/V816.0.2.0.UGPMIXM:user/release-keys"
 };
 
-static const variant_info_t gale_la_info = {
+static const variant_info_t c3ul_info = {
     .hwc_value = "",
-    .sku_value = "gale_la",
+    .sku_value = "c3ul",
 
     .brand = "Redmi",
     .device = "gale",
+    .mod_device = "gale_global",
     .marketname = "Redmi 13C",
     .model = "23100RN82L",
     .build_fingerprint = "Redmi/gale_global/gale:13/SP1A.210812.016/V816.0.2.0.UGPMIXM:user/release-keys"
 };
 
-static const variant_info_t gale_in_info = {
+static const variant_info_t c3uin_info = {
     .hwc_value = "",
-    .sku_value = "gale_in",
+    .sku_value = "c3uin",
 
     .brand = "Redmi",
     .device = "gale",
+    .mod_device = "gale_global",
     .marketname = "Redmi 13C",
     .model = "2311DRN14I",
     .build_fingerprint = "Redmi/gale_global/gale:13/SP1A.210812.016/V816.0.2.0.UGPMIXM:user/release-keys"
 };
 
-static const variant_info_t gust_p_info = {
+static const variant_info_t c3upg_info = {
     .hwc_value = "",
-    .sku_value = "gust_p",
+    .sku_value = "c3upg",
 
     .brand = "Poco",
     .device = "gust",
+    .mod_device = "gust_p_global",
     .marketname = "POCO C65",
     .model = "2310FPCA4G",
     .build_fingerprint = "POCO/gale_p_global/gale:13/SP1A.210812.016/V816.0.2.0.UGPMIXM:user/release-keys"
 };
 
-static const variant_info_t gale_p_info = {
+static const variant_info_t c3upin_info = {
     .hwc_value = "",
-    .sku_value = "gale_p",
+    .sku_value = "c3upin",
 
     .brand = "Poco",
     .device = "gale",
+    .mod_device = "gale_p_global",
     .marketname = "POCO C65",
     .model = "2310FPCA4I",
     .build_fingerprint = "POCO/gale_p_global/gale:13/SP1A.210812.016/V816.0.2.0.UGPMIXM:user/release-keys"
 };
 
 static const std::vector<variant_info_t> variants = {
-    gust_info,
-    gale_info,
-    gale_la_info,
-    gale_in_info,
-    gale_p_info,
-    gust_p_info,
+    c3un_info,
+    c3ua_info,
+    c3ul_info,
+    c3upin_info,
+    c3uin_info,
+    c3upg_info,
 };
 
 void vendor_load_properties() {
--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -12,7 +12,7 @@
 
 using android::base::GetProperty;
 
-#define HWC_PROP "ro.boot.hwc"
+#define HWC_PROP "ro.boot.rsc"
 #define SKU_PROP "ro.boot.product.hardware.sku"
 
 void search_variant(const std::vector<variant_info_t> variants) {
@@ -34,9 +34,15 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("marketname", variant.marketname, true);
     set_ro_build_prop("model", variant.model, true);
 
+    property_override("ro.product.mod_device", variant.mod_device, true);
+
+    property_override("bluetooth.device.default_name", variant.marketname, true);
+
     property_override("vendor.usb.product_string", variant.marketname, true);
     property_override("bluetooth.device.default_name", variant.marketname, true);
     set_ro_build_prop("fingerprint", variant.build_fingerprint);
     property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
     property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
+
+    property_override("ro.product.vendor.name", variant.mod_device, true);
 }
