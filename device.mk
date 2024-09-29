823ab68f0a980cce64d4258f5de52e106d902370
Demon000 <demonsingur@gmail.com>
2024-09-29 18:57:22 -0500

    gale: sensors: Avoid target name conflicts

--- device.mk
+++ device.mk
@@ -344,7 +344,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors-service.multihal
+    android.hardware.sensors@2.1-service.gale-multihal
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
