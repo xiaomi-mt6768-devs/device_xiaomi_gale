6018644b702b9b7d6ecc3cb732acec18f6a40264
sabrina010209 <mcpe11640@gmail.com>
2024-10-02 17:11:33 -0500

    gale: Build `libnetutils.vendor`

--- device.mk
+++ device.mk
@@ -385,6 +385,9 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
 
+PRODUCT_PACKAGES += \
+    libnetutils.vendor
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
