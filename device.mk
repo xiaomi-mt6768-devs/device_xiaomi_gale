23fe73fe63e80989a3e1706238873813bf6a8ed2
claxten10 <claxten10@gmail.com>
2024-10-07 14:17:42 +0000

    gale: Uprev Graphics Composer to 2.3

--- device.mk
+++ device.mk
@@ -104,8 +104,8 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.memtrack-service.mediatek-mali 
+    android.hardware.graphics.composer@2.3-service \
+    android.hardware.memtrack-service.mediatek-mali
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
