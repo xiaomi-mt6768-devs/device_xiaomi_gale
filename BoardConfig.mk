8fcc741d4412a82a4ea02e5c539b40b8c9d8981e
sabrina010209 <mcpe11640@gmail.com>
2024-11-16 21:32:18 -0500

    gale: sepolicy: Fix genfs patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SPL
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
