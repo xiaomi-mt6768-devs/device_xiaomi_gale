c5fdbc4b47728f8c2a90cdf3afc10c91d09f7b29
sabrina010209 <mcpe11640@gmail.com>
2024-11-27 08:26:39 +0530

    gale: Fixup sepolicy flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,7 +136,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SPL
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
