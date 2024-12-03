627ee608ea2ce4a02eed60b04c630749ca0a8c50
basamaryan <basam.aryan@gmail.com>
2024-12-03 01:50:23 -0500

    gale: Move to Xiaomi fingerprint AIDL

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     vendor.xiaomi.hardware.fx.tunnel@1.0.vendor \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
