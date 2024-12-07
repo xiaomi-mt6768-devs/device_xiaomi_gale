3531ff101bf0c2e11931aac1812f82630caf84ce
sabrina010209 <mcpe11640@gmail.com>
2024-12-07 14:10:28 -0500

    gale: Switch to prebuilt composer
    * Our composer version was update to 3.3 in the latest hyperOS

--- device.mk
+++ device.mk
@@ -82,7 +82,6 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
     android.hardware.memtrack-service.mediatek-mali
 
 PRODUCT_PACKAGES += \
